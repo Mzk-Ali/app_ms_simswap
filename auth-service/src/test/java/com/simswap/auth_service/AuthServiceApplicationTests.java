d0346464a749c89d0124f7585f1537f6dbf55bff
Mzk-Ali <ali.marzak@live.fr>
2025-11-02 19:22:33 +0100

    🐛 FIX: Correction CI / CD

--- auth-service/src/test/java/com/simswap/auth_service/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/simswap/auth_service/AuthServiceApplicationTests.java
@@ -1,12 +1,44 @@
 package com.simswap.auth_service;
 
+import static org.mockito.Mockito.mock;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+
+import com.simswap.auth_service.services.JwtService;
+import com.simswap.auth_service.services.TokenService;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class AuthServiceApplicationTests {
+	@TestConfiguration
+    static class TestConfig {
+
+        @Bean
+        public AuthenticationManager authenticationManager() {
+            return email -> null;
+        }
+
+        @Bean
+        public JwtService jwtService() {
+            return mock(JwtService.class);
+        }
+
+        @Bean
+        public TokenService tokenService() {
+            return mock(TokenService.class);
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return mock(PasswordEncoder.class);
+        }
+    }
 
 	@Test
 	void contextLoads() {
