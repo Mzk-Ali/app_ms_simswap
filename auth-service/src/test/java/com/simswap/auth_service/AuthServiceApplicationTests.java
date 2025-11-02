7128f9a58d47bced724eaf6c060eb193b33270ca
Mzk-Ali <ali.marzak@live.fr>
2025-11-02 19:30:29 +0100

    🐛 FIX: Correction CI / CD

--- auth-service/src/test/java/com/simswap/auth_service/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/simswap/auth_service/AuthServiceApplicationTests.java
@@ -3,6 +3,9 @@ package com.simswap.auth_service;
 import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +18,10 @@ import com.simswap.auth_service.services.JwtService;
 import com.simswap.auth_service.services.TokenService;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = { 
+	    DataSourceAutoConfiguration.class, 
+	    HibernateJpaAutoConfiguration.class 
+	})
 @ActiveProfiles("test")
 class AuthServiceApplicationTests {
 	@TestConfiguration
